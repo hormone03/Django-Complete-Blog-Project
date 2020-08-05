801abffb4ad6dd91e6c6f77fb336f498c32eae77
hormone03 <hormone03@yahoo.com>
2020-08-05 02:30:10 +0200

    Set debug value

--- django_project/urls.py
+++ django_project/urls.py
@@ -47,7 +47,7 @@ urlpatterns = [
          ),
          name='password_reset_complete'),
     path('', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
